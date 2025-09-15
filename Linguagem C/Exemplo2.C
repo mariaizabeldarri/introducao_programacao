08dd8672d19169f356d826fe568645c2f4ce8d7a
mariaizabeldarri <maria.darri.carvalho@escola.pr.gov.br>
2025-09-15 08:07:13 -0300

    Create Exemplo2.C

---
+++ Linguagem C/Exemplo2.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int a=10,b=3;
+  
+    printf ("Soma: %d\n",a+b);
+    printf ("Subtração: %d\n",a-b);
+    printf ("Multiplicação: %d\n",a*b);
+    printf ("Divisão: %d\n",a/b);
+    printf ("Módulo(resto da divisão): %d\n",a%b);
+    
+    float c=10.5, d=3.0;
+    printf("Divisão: %.2f\n", c/d);
+    return 0;
+}
